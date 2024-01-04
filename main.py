29f516f1404c4cbb175ace86ad7e27899350a1f3
Erick Díaz <erickdp@hotmail.com>
2024-01-04 10:59:53 -0500

    update api gpt call

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-function = lambda x: x ** 2
+from dotenv import load_dotenv
 
-print(list(map(function, range(10))))
+import os
+from loguru import logger
 
+from ec.llm.service.inference_service import InferenceService
 
+# function = lambda x: x ** 2
+#
+# print(list(map(function, range(10))))
+
+load_dotenv('./secrets/.env-dev')
+
+print(os.getenv('OPENAI_API_KEY', 'kepler'))
+
+service = InferenceService()
+logger.info(service.invoke('2020'))
